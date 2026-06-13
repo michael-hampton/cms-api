a85ed30e2470325a9dbc705adbf808c604953a52
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-13 21:31:27 +0100

    Add contributor brief rejection form request

---
+++ src/Requests/OpenCollab/Briefs/RejectBriefAssignmentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Requests\OpenCollab\Briefs;
+
+use App\Framework\Http\FormRequest;
+
+class RejectBriefAssignmentRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'reason' => ['required', 'string', 'max:1000'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'reason.required' => 'Reason is required.',
+            'reason.max' => 'Reason must be 1000 characters or fewer.',
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->has('reason')) {
+            $this->merge(['reason' => trim((string) $this->input('reason'))]);
+        }
+    }
+}
