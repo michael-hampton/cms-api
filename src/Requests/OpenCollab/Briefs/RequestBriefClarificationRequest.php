dcb790eb6cc3ef610f8b71958c420b62b4f40fa7
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-13 21:31:32 +0100

    Add contributor clarification form request

---
+++ src/Requests/OpenCollab/Briefs/RequestBriefClarificationRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Requests\OpenCollab\Briefs;
+
+use App\Framework\Http\FormRequest;
+
+class RequestBriefClarificationRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'message' => ['required', 'string', 'max:3000'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'message.required' => 'Message is required.',
+            'message.max' => 'Message must be 3000 characters or fewer.',
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->has('message')) {
+            $this->merge(['message' => trim((string) $this->input('message'))]);
+        }
+    }
+}
