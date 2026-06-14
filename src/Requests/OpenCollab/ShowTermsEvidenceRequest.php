120c12e35640a2437b39da2d3a834b9ae90b3265
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-14 20:15:28 +0100

    Add terms evidence request

---
+++ src/Requests/OpenCollab/ShowTermsEvidenceRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Requests\OpenCollab;
+
+use App\Framework\Http\FormRequest;
+
+class ShowTermsEvidenceRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
