c7a1deb7e8ba7a2b38585976ee67d837ba0cd3a1
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 21:36:20 +0100

    Expose public content access state

--- src/DTO/PublicContent/PublicContentDocument.php
+++ src/DTO/PublicContent/PublicContentDocument.php
@@ -19,6 +19,7 @@ final readonly class PublicContentDocument
         public array $landingSections = [],
         public array $links = [],
         public array $widgets = [],
+        public array $access = ['can_view' => true, 'reason' => null],
         public string $schemaVersion = '1.0',
     ) {
     }
