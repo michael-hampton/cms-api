5aad62547fcb3a9799d6d9083cd146fb98a8313b
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 14:18:23 +0100

    Load public content preview route before legacy routes

--- src/config/routing.php
+++ src/config/routing.php
@@ -10,6 +10,7 @@ return [
     |
     */
     'files' => [
+        'routes/public-content-preview.php',
         'routes/web.php',
         'routes/api.php',
         'routes/public-content-api.php',
