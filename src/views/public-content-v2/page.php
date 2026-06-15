fa330d16db32087e9cdd9b3561247d458c291c0a
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 19:02:05 +0100

    Load V2 search result navigation override

--- src/views/public-content-v2/page.php
+++ src/views/public-content-v2/page.php
@@ -37,6 +37,7 @@ $description = $preview ? 'Public content V2 preview' : ($pageDescription ?? '')
 @js('public-content-v2-member-hub-loader.js')
 <?php if ($preview): ?>
     @js('public-content-v2-preview-links.js')
+    @js('public-content-v2-search.js')
 <?php else: ?>
     @js('public-content-v2-production-links.js')
 <?php endif; ?>
