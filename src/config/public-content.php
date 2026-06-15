30551fad587f6bd8aedd16f795e861efba9f3f44
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 21:08:41 +0100

    Add widget extension configuration

--- src/config/public-content.php
+++ src/config/public-content.php
@@ -5,7 +5,8 @@ return [
     'preview_enabled' => env('PUBLIC_CONTENT_V2_PREVIEW_ENABLED', true),
     'shadow_enabled' => env('PUBLIC_CONTENT_V2_SHADOW_ENABLED', false),
     'site_ids' => [],
-    'page_types' => ['page', 'article', 'landing-page'],
+    'page_types' => ['page', 'content', 'article', 'landing-page'],
+    'widgets' => [],
     'cache' => [
         'public_ttl_seconds' => 300,
         'viewer_state' => 'private, no-store',
