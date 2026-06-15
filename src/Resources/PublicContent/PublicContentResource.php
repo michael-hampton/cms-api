0e74de4f010b3664a20c729a91ec1cb57ff0cd44
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 15:03:45 +0100

    Expose composed API component regions

--- src/Resources/PublicContent/PublicContentResource.php
+++ src/Resources/PublicContent/PublicContentResource.php
@@ -22,14 +22,19 @@ final readonly class PublicContentResource
             'seo' => $this->document->seo,
             'taxonomy' => $this->document->taxonomy,
             'authors' => $this->document->authors,
-            'landing_sections' => $this->document->landingSections,
             'links' => $this->document->links,
-            'widgets' => $this->document->widgets,
             'content' => [
                 'schema_version' => $this->document->schemaVersion,
                 'regions' => array_map(
                     static fn($region) => $region->toArray(),
                     $this->document->regions,
+                ),
+                'components' => array_map(
+                    static fn(array $components): array => array_map(
+                        static fn($component): array => $component->toArray(),
+                        $components,
+                    ),
+                    $this->document->components,
                 ),
             ],
         ];
