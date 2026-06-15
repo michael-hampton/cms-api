973894ec4ca96b4d0dc7c260a1606690ac382883
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 09:40:47 +0100

    Pass terms UI component permissions to view

--- src/Controllers/OpenCollab/Admin/AdminTermsPageController.php
+++ src/Controllers/OpenCollab/Admin/AdminTermsPageController.php
@@ -4,6 +4,8 @@ namespace App\Controllers\OpenCollab\Admin;
 
 use App\Controllers\Controller;
 use App\Controllers\OpenCollab\Concerns\AuthorizesSitePagePermissions;
+use App\Controllers\OpenCollab\Concerns\ResolvesUiComponents;
+use App\Framework\Authorization\Auth;
 use App\Framework\Http\Request;
 use App\Framework\Support\SiteContext;
 use App\Repositories\OpenCollab\TermsVersionRepository;
@@ -12,6 +14,7 @@ use App\Services\OpenCollab\OpenCollabAuthorizationService;
 class AdminTermsPageController extends Controller
 {
     use AuthorizesSitePagePermissions;
+    use ResolvesUiComponents;
 
     public function __construct(
         private readonly TermsVersionRepository $repository,
@@ -22,7 +25,7 @@ class AdminTermsPageController extends Controller
 
     public function index(Request $request)
     {
-        if ($response = $this->authorizeSitePagePermissions(['terms.edit', 'terms.publish', 'terms.archive'])) {
+        if ($response = $this->authorizeSitePagePermissions(['terms.view', 'terms.create', 'terms.publish'])) {
             return $response;
         }
 
@@ -30,10 +33,16 @@ class AdminTermsPageController extends Controller
         $selectedId = (int)$request->input('terms_id', 0);
 
         return $this->view('open-collab.admin.terms.index', [
+            'allowedComponentKeys' => $this->allowedUiComponentKeysForSurface('terms.index'),
             'termsVersions' => $this->repository->allForSite($siteId),
             'selectedTerms' => $selectedId > 0
                 ? $this->repository->findForSite($selectedId, $siteId)
                 : null,
+            'pageTitle' => 'Terms & Conditions',
+            'activeNav' => 'terms',
+            'breadcrumbs' => [['label' => 'Terms & Conditions']],
+            'currentUser' => Auth::user(),
+            'site' => SiteContext::slug(),
             'siteSlug' => SiteContext::slug(),
         ]);
     }
