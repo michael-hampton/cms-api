a6bf1dfd7ebfd6e1e76a3d76c86dfa49b0211b25
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 16:42:47 +0100

    Treat missing V2 slug as homepage

--- src/routes/public-content-preview.php
+++ src/routes/public-content-preview.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Controllers\Front\PublicContentHomepagePreviewController;
 use App\Controllers\Front\PublicContentPreviewController;
 use App\Framework\Http\Router;
 
 /** @var Router $router */
+$router->get(
+    '/{site}/content-v2',
+    [PublicContentHomepagePreviewController::class, 'show'],
+);
+
 $router->get(
     '/{site}/content-v2/{slug}',
     [PublicContentPreviewController::class, 'show'],
