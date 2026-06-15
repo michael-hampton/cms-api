4e8a508cb5db103cd252fa0430b9ff97f94e32f1
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 21:07:23 +0100

    Add public content widget provider

---
+++ src/Providers/PublicContentWidgetServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Framework\ServiceProvider\ServiceProvider;
+use App\Framework\Support\Config;
+use App\Services\PublicContent\Widgets\PublicContentWidgetDefinition;
+use App\Services\PublicContent\Widgets\PublicContentWidgetRegistry;
+
+final class PublicContentWidgetServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->container->singleton(PublicContentWidgetRegistry::class);
+    }
+
+    public function boot(): void
+    {
+        $registry = $this->container->resolve(PublicContentWidgetRegistry::class);
+
+        foreach (Config::get('public-content.widgets', []) as $className) {
+            $widget = $this->container->resolve($className);
+
+            if ($widget instanceof PublicContentWidgetDefinition) {
+                $registry->register($widget);
+            }
+        }
+    }
+}
