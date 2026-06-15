c3d56f3ecc8252f9faa4012e656836938d58b2b7
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 14:38:32 +0100

    Add public content rollout configuration

---
+++ src/config/public-content.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'enabled' => env('PUBLIC_CONTENT_V2_ENABLED', false),
+    'preview_enabled' => env('PUBLIC_CONTENT_V2_PREVIEW_ENABLED', true),
+    'shadow_enabled' => env('PUBLIC_CONTENT_V2_SHADOW_ENABLED', false),
+    'site_ids' => [],
+    'page_types' => ['page', 'article', 'landing-page'],
+    'cache' => [
+        'public_ttl_seconds' => 300,
+        'viewer_state' => 'private, no-store',
+    ],
+];
