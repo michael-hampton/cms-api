939185d9f4d63c798bc733dfc26cfad715a80c93
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 14:49:52 +0100

    Gate public content preview

--- src/Controllers/Front/PublicContentPreviewController.php
+++ src/Controllers/Front/PublicContentPreviewController.php
@@ -5,16 +5,21 @@ namespace App\Controllers\Front;
 use App\Controllers\Controller;
 use App\Framework\Http\Response;
 use App\Framework\Support\SiteContext;
+use App\Services\PublicContent\PublicContentRollout;
 
 final class PublicContentPreviewController extends Controller
 {
-    public function __construct()
+    public function __construct(private readonly PublicContentRollout $rollout)
     {
         parent::__construct();
     }
 
     public function show(string $slug): Response
     {
+        if (!$this->rollout->previewEnabled()) {
+            return $this->notFound('Public content preview is disabled.');
+        }
+
         $siteSlug = SiteContext::slug();
 
         return $this->view('public-content-v2/page', [
