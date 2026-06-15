31c8e1035a78219ad252b701fd21a83ed06d6c06
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-15 14:07:36 +0100

    Add public content API resource

---
+++ src/Resources/PublicContent/PublicContentResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Resources\PublicContent;
+
+use App\DTO\PublicContent\PublicContentDocument;
+
+final readonly class PublicContentResource
+{
+    public function __construct(private PublicContentDocument $document)
+    {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->document->id,
+            'site_id' => $this->document->siteId,
+            'slug' => $this->document->slug,
+            'type' => $this->document->type,
+            'title' => $this->document->title,
+            'summary' => $this->document->summary,
+            'seo' => $this->document->seo,
+            'taxonomy' => $this->document->taxonomy,
+            'content' => [
+                'schema_version' => $this->document->schemaVersion,
+                'regions' => array_map(
+                    static fn($region) => $region->toArray(),
+                    $this->document->regions,
+                ),
+            ],
+        ];
+    }
+}
