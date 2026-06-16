4d2d93b1b4fa1a3f2b6174f4da5ce08021d485b7
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-16 15:27:05 +0100

    Register editorial content preview routes

--- src/routes/public-content-preview.php
+++ src/routes/public-content-preview.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\Front\EditorialPublicContentPreviewController;
 use App\Controllers\Front\PublicContentHomepagePreviewController;
 use App\Controllers\Front\PublicContentPreviewController;
 use App\Framework\Http\Router;
@@ -8,6 +9,16 @@ use App\Framework\Http\Router;
 $router->get(
     '/{site}/content-v2',
     [PublicContentHomepagePreviewController::class, 'show'],
+);
+
+$router->get(
+    '/{site}/content-v2/editorial-preview/{pageId}',
+    [EditorialPublicContentPreviewController::class, 'show'],
+);
+
+$router->get(
+    '/api/v1/{site}/editorial-preview/{pageId}',
+    [EditorialPublicContentPreviewController::class, 'data'],
 );
 
 $router->get(
