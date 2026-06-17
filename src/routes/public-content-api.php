f17ca48f8dd16bde56458851700f41f2b06d0398
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-17 17:42:20 +0100

    Apply latency measurement to public API routes

--- src/routes/public-content-api.php
+++ src/routes/public-content-api.php
@@ -4,14 +4,19 @@ use App\Controllers\Api\V1\PublicContentBadgeModalController;
 use App\Controllers\Api\V1\PublicContentController;
 use App\Controllers\Api\V1\PublicContentViewerController;
 use App\Framework\Http\Router;
+use App\Middleware\PublicContent\MeasurePublicApiLatencyMiddleware;
 
 /** @var Router $router */
-$router->get('/api/v1/{site}/content/{slug}', [PublicContentController::class, 'show']);
-$router->get('/api/v1/{site}/regions/{regionSlug}/content/{slug}', [PublicContentController::class, 'showRegional']);
-$router->get('/api/v1/{site}/content/{pageId}/viewer-state', [PublicContentViewerController::class, 'show']);
-$router->put('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'like']);
-$router->delete('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'unlike']);
-$router->post('/api/v1/{site}/content/{pageId}/views', [PublicContentViewerController::class, 'recordView']);
-$router->get('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'comments']);
-$router->post('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'storeComment']);
-$router->post('/api/v1/{site}/badge-modals/{memberBadgeId}/viewed', [PublicContentBadgeModalController::class, 'markViewed']);
+$router->group([
+    'middleware' => [MeasurePublicApiLatencyMiddleware::class],
+], function (Router $router): void {
+    $router->get('/api/v1/{site}/content/{slug}', [PublicContentController::class, 'show']);
+    $router->get('/api/v1/{site}/regions/{regionSlug}/content/{slug}', [PublicContentController::class, 'showRegional']);
+    $router->get('/api/v1/{site}/content/{pageId}/viewer-state', [PublicContentViewerController::class, 'show']);
+    $router->put('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'like']);
+    $router->delete('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'unlike']);
+    $router->post('/api/v1/{site}/content/{pageId}/views', [PublicContentViewerController::class, 'recordView']);
+    $router->get('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'comments']);
+    $router->post('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'storeComment']);
+    $router->post('/api/v1/{site}/badge-modals/{memberBadgeId}/viewed', [PublicContentBadgeModalController::class, 'markViewed']);
+});
