976b40ab74a680c0063aa1e453e56d550aec3810
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-17 17:01:59 +0100

    Persist badge modal viewed state

--- src/Models/MemberBadge.php
+++ src/Models/MemberBadge.php
@@ -7,14 +7,19 @@ class MemberBadge extends Model
     protected $table = 'member_badges';
 
     protected $fillable = [
-        'member_id', 'badge_id', 'earned_at',
-        'criteria_met', 'is_visible'
+        'member_id',
+        'badge_id',
+        'earned_at',
+        'criteria_met',
+        'is_visible',
+        'modal_viewed_at',
     ];
 
     protected $casts = [
         'earned_at' => 'datetime',
         'criteria_met' => 'array',
-        'is_visible' => 'boolean'
+        'is_visible' => 'boolean',
+        'modal_viewed_at' => 'datetime',
     ];
 
     public function member($relation = false)
