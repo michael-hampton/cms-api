d3adddd92c1431da5f56bc126a5b01bd455a9aa9
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-18 17:25:44 +0100

    Configure public content widget visibility by page type

---
+++ src/config/public_content.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'widgets' => [
+        'page-title' => [
+            'page_types' => ['article'],
+        ],
+        'category-pills' => [
+            'page_types' => ['article'],
+        ],
+        'tags' => [
+            'page_types' => ['article'],
+        ],
+        'page-actions' => [
+            'page_types' => ['article'],
+        ],
+    ],
+];
