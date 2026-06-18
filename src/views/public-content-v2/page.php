21412b0425762443dff91c47c7f9e33d2cd5c126
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-18 16:59:35 +0100

    Remove duplicate asset include

--- src/views/public-content-v2/page.php
+++ src/views/public-content-v2/page.php
@@ -45,7 +45,6 @@ $csrfToken = \App\Framework\Security\Csrf::getToken();
 @css('member-hub.css')
 @css('public-content-v2.css')
 @js('base.js')
-@js('member-hub.js')
 <?php if ($preview): ?>
     @js('public-content-v2-preview-links.js')
     @js('public-content-v2-search.js')
