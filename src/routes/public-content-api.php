a00b8123f798026ba39c554b990a0d49a1ac41db
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-18 13:56:25 +0100

    Harden public content API middleware stack

--- src/routes/public-content-api.php
+++ src/routes/public-content-api.php
@@ -6,20 +6,32 @@ use App\Controllers\Api\V1\PublicContentViewerController;
 use App\Framework\Http\Router;
 use App\Framework\Middleware\VerifyCsrfToken;
 use App\Middleware\PublicContent\MeasurePublicApiLatencyMiddleware;
+use App\Middleware\PublicContent\PublicApiCorsMiddleware;
+use App\Middleware\PublicContent\PublicApiRateLimitMiddleware;
+use App\Middleware\PublicContent\RequireMemberAuthMiddleware;
+use App\Middleware\PublicContent\SecurityHeadersMiddleware;
+use App\Middleware\PublicContent\ValidatePublicApiQueryMiddleware;
 
 /** @var Router $router */
 $router->group([
-    'middleware' => [MeasurePublicApiLatencyMiddleware::class],
+    'middleware' => [
+        SecurityHeadersMiddleware::class,
+        PublicApiCorsMiddleware::class,
+        PublicApiRateLimitMiddleware::class,
+        ValidatePublicApiQueryMiddleware::class,
+        MeasurePublicApiLatencyMiddleware::class,
+    ],
 ], function (Router $router): void {
     $csrf = [VerifyCsrfToken::class];
+    $memberMutation = [RequireMemberAuthMiddleware::class, VerifyCsrfToken::class];
 
     $router->get('/api/v1/{site}/content/{slug}', [PublicContentController::class, 'show']);
     $router->get('/api/v1/{site}/regions/{regionSlug}/content/{slug}', [PublicContentController::class, 'showRegional']);
     $router->get('/api/v1/{site}/content/{pageId}/viewer-state', [PublicContentViewerController::class, 'show']);
-    $router->put('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'like'], middleware: $csrf);
-    $router->delete('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'unlike'], middleware: $csrf);
+    $router->put('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'like'], middleware: $memberMutation);
+    $router->delete('/api/v1/{site}/content/{pageId}/like', [PublicContentViewerController::class, 'unlike'], middleware: $memberMutation);
     $router->post('/api/v1/{site}/content/{pageId}/views', [PublicContentViewerController::class, 'recordView']);
     $router->get('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'comments']);
     $router->post('/api/v1/{site}/content/{pageId}/comments', [PublicContentViewerController::class, 'storeComment'], middleware: $csrf);
-    $router->post('/api/v1/{site}/badge-modals/{memberBadgeId}/viewed', [PublicContentBadgeModalController::class, 'markViewed'], middleware: $csrf);
+    $router->post('/api/v1/{site}/badge-modals/{memberBadgeId}/viewed', [PublicContentBadgeModalController::class, 'markViewed'], middleware: $memberMutation);
 });
