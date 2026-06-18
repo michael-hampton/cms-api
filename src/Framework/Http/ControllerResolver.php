401c65e751b7023eb84844632e190be85760aa5b
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-18 17:35:48 +0100

    Route public page types through API-first controller

--- src/Framework/Http/ControllerResolver.php
+++ src/Framework/Http/ControllerResolver.php
@@ -11,16 +11,16 @@ class ControllerResolver implements ControllerResolverInterface
     ) {
         $this->controllerMappings = array_merge([
             'page' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
-            'article' => 'App\Controllers\Front\ContentController@show',
-            'content' => 'App\Controllers\Front\ContentController@show',
-            'landing-page' => 'App\Controllers\Front\ContentController@show',
-            'buying-guide' => 'App\Controllers\Front\ContentController@show',
-            'gallery' => 'App\Controllers\Front\ContentController@show',
-            'review' => 'App\Controllers\Front\ContentController@show',
-            'blog' => 'App\Controllers\Front\ContentController@show',
+            'article' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'content' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'landing-page' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'buying-guide' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'gallery' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'review' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
+            'blog' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
             'product' => 'App\Controllers\ProductController@show',
             'category' => 'App\Controllers\CategoryController@show',
-            'event' => 'App\Controllers\Front\ContentController@show',
+            'event' => 'App\Controllers\Front\ApiFirstPublicContentController@show',
             'documentation' => 'App\Controllers\DocsController@show',
         ], $this->controllerMappings);
     }
@@ -31,15 +31,11 @@ class ControllerResolver implements ControllerResolverInterface
             return $page->controller;
         }
 
-        if (isset($this->controllerMappings[$page->page_type])) {
-            return $this->controllerMappings[$page->page_type];
-        }
-
         if ($page->custom_handler) {
             return $page->custom_handler;
         }
 
-        return null;
+        return $this->controllerMappings[$page->page_type] ?? null;
     }
 
     public function shouldUseController(Page $page): bool
