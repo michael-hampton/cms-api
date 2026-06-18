0c68af3b9b5a3c38485e5a63b5b28a576442bc1c
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-18 21:10:37 +0100

    Configure public media CDN settings

--- src/config/public-content.php
+++ src/config/public-content.php
@@ -13,4 +13,8 @@ return [
         'public_ttl_seconds' => 300,
         'viewer_state' => 'private, no-store',
     ],
+    'media' => [
+        'signed_ttl_seconds' => env('PUBLIC_MEDIA_SIGNED_TTL_SECONDS', 86400),
+        'document_root' => env('PUBLIC_MEDIA_DOCUMENT_ROOT', null),
+    ],
 ];
