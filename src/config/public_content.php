650e2b09e418d76740b3f2edffe9b1525d70d8e1
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-19 12:37:03 +0100

    Enable design tokens for all public content page types

--- src/config/public_content.php
+++ src/config/public_content.php
@@ -2,6 +2,7 @@
 
 return [
     'widgets' => [
+        'design-tokens' => ['page_types' => ['*']],
         'page-title' => ['page_types' => ['article']],
         'category-pills' => ['page_types' => ['article']],
         'tags' => ['page_types' => ['article']],
