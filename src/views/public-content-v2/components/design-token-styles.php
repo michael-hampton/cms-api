85419302fb84de4ed19555157601bfceccb3644e
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-19 12:42:29 +0100

    Remove indirect design token style partial

--- src/views/public-content-v2/components/design-token-styles.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($cssVariables)): ?>
-<style data-public-content-design-tokens="<?= htmlspecialchars((string) $siteSlug, ENT_QUOTES, 'UTF-8') ?>">
-:root {
-<?php foreach ($cssVariables as $name => $value): ?>
-    <?= htmlspecialchars((string) $name, ENT_QUOTES, 'UTF-8') ?>: <?= htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8') ?>;
-<?php endforeach; ?>
-}
-</style>
-<?php endif; ?>
