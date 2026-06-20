0502ac80fbfc8dc2f1398755e73f705d4605a848
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-20 17:42:54 +0100

    Bootstrap Stripe for subscription upgrades

--- src/views/subscriptions/account/subscriptions.php
+++ src/views/subscriptions/account/subscriptions.php
@@ -4,6 +4,12 @@
     @include('subscriptions/shared/_subscription_account')
 </main>
 </div>
+<script>
+    window.SubscriptionAccountStripeKey = <?= json_encode(
+        $_ENV['STRIPE_PUBLIC_KEY'] ?? config('payment.stripe.public_key')
+    ) ?>;
+</script>
+<script src="https://js.stripe.com/v3/" defer></script>
 <script src="/public/js/subscription-account.js" defer></script>
 <script src="/public/js/subscription-account-runtime.js" defer></script>
 <script src="/public/js/subscription-account-drawer-bootstrap.js" defer></script>
