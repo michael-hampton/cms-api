d497984b4152289a5cfba9ed6247cfd9b68301c6
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-20 15:48:51 +0100

    Remove subscription account routes from public directory routes

--- src/routes/public-directory.php
+++ src/routes/public-directory.php
@@ -2,52 +2,9 @@
 
 use App\Controllers\Api\V1\PublicDirectoryController;
 use App\Controllers\Front\PublicDirectoryPageController;
-use App\Controllers\Members\Subscriptions\UnifiedMemberSubscriptionsController;
-use App\Controllers\Subscription\ShopAccountApiController;
-use App\Controllers\Subscription\ShopAccountBillingDatePreviewController;
-use App\Controllers\Subscription\ShopAccountBillingDateUpdateController;
-use App\Controllers\Subscription\ShopAccountController;
-use App\Controllers\Subscription\ShopAccountDeliveryAddressController;
-use App\Controllers\Subscription\ShopAccountDeliveryController;
-use App\Controllers\Subscription\ShopAccountIssueDeliveryController;
-use App\Controllers\Subscription\ShopAccountSubscriptionHistoryController;
-use App\Controllers\Subscription\ShopAccountSubscriptionPreferenceController;
-use App\Controllers\Subscription\ShopAccountSubscriptionSettingsController;
-use App\Controllers\Subscription\ShopAccountSubscriptionUpgradeController;
 use App\Framework\Http\Router;
-use App\Framework\Middleware\AuthenticateMemberWithToken;
-use App\Framework\Middleware\RequireSubscriptionAccountAccess;
-use App\Framework\Middleware\VerifyCsrfToken;
 
 /** @var Router $router */
-
-$auth = [AuthenticateMemberWithToken::class];
-$owned = [AuthenticateMemberWithToken::class, RequireSubscriptionAccountAccess::class];
-$write = [AuthenticateMemberWithToken::class, RequireSubscriptionAccountAccess::class, VerifyCsrfToken::class];
-
-$router->get('/{site}/member/subscriptions/unified', UnifiedMemberSubscriptionsController::class, middleware: $auth);
-$router->post('/{site}/member/subscriptions/{id}/cancel', [ShopAccountApiController::class, 'cancelSubscription'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/reactivate', [ShopAccountApiController::class, 'reactivateSubscription'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/pause', [ShopAccountApiController::class, 'pauseSubscription'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/resume', [ShopAccountApiController::class, 'resumeSubscription'], middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/renew', [ShopAccountController::class, 'renew'], middleware: $owned);
-$router->get('/{site}/member/subscriptions/{id}/resubscribe', [ShopAccountController::class, 'resubscribe'], middleware: $owned);
-$router->get('/{site}/member/subscriptions/{id}/settle-payment', [ShopAccountApiController::class, 'settlePayment'], middleware: $owned);
-$router->post('/{site}/member/subscriptions/{id}/auto-renew', [ShopAccountSubscriptionSettingsController::class, 'updateAutoRenew'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/billing-date/preview', ShopAccountBillingDatePreviewController::class, middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/billing-date', ShopAccountBillingDateUpdateController::class, middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/history', ShopAccountSubscriptionHistoryController::class, middleware: $owned);
-$router->get('/{site}/member/subscriptions/{id}/delivery', [ShopAccountDeliveryController::class, 'status'], middleware: $owned);
-$router->post('/{site}/member/subscriptions/{id}/delivery/pause', [ShopAccountDeliveryController::class, 'pause'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/delivery/resume', [ShopAccountDeliveryController::class, 'resume'], middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/upgrades', [ShopAccountSubscriptionUpgradeController::class, 'options'], middleware: $owned);
-$router->post('/{site}/member/subscriptions/{id}/upgrades/preview', [ShopAccountSubscriptionUpgradeController::class, 'preview'], middleware: $write);
-$router->post('/{site}/member/subscriptions/{id}/upgrades', [ShopAccountSubscriptionUpgradeController::class, 'upgrade'], middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/preferences', [ShopAccountSubscriptionPreferenceController::class, 'show'], middleware: $owned);
-$router->post('/{site}/member/subscriptions/{id}/preferences', [ShopAccountSubscriptionPreferenceController::class, 'update'], middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/delivery-addresses', [ShopAccountDeliveryAddressController::class, 'index'], middleware: $owned);
-$router->post('/{site}/member/subscriptions/{id}/delivery-addresses/{addressId}/default', [ShopAccountDeliveryAddressController::class, 'setDefault'], middleware: $write);
-$router->get('/{site}/member/subscriptions/{id}/issue-deliveries', ShopAccountIssueDeliveryController::class, middleware: $owned);
 
 $router->get('/{site}/content-v2/authors', [PublicDirectoryPageController::class, 'previewAuthors']);
 $router->get('/{site}/content-v2/authors/{slug}', [PublicDirectoryPageController::class, 'previewAuthor']);
