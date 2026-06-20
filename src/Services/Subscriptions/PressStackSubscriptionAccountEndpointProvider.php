0614e2832743c612c47949d3a8746b59e5ee9b62
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-20 14:25:46 +0100

    Complete PressStack subscription endpoint mapping

--- src/Services/Subscriptions/PressStackSubscriptionAccountEndpointProvider.php
+++ src/Services/Subscriptions/PressStackSubscriptionAccountEndpointProvider.php
@@ -15,8 +15,15 @@ final class PressStackSubscriptionAccountEndpointProvider implements Subscriptio
             'billing_date_preview_endpoint' => $base . '/billing-date/preview',
             'billing_date_update_endpoint' => $base . '/billing-date',
             'history_endpoint' => $base . '/history',
+            'upgrade_options_endpoint' => $base . '/upgrades',
+            'upgrade_preview_endpoint' => $base . '/upgrades/preview',
+            'upgrade_endpoint' => $base . '/upgrades',
             'preference_endpoint' => $base . '/preferences',
             'delivery_status_endpoint' => $base . '/delivery',
+            'delivery_pause_endpoint' => $base . '/delivery/pause',
+            'delivery_resume_endpoint' => $base . '/delivery/resume',
+            'delivery_address_endpoint' => $base . '/delivery-addresses',
+            'delivery_address_update_endpoint' => $base . '/delivery-addresses/__ADDRESS_ID__/default',
             'issue_delivery_endpoint' => $base . '/issue-deliveries',
         ];
     }
