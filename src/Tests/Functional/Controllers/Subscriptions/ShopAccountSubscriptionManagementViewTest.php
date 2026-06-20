4cdbde5297e76e2c6f4c03994c97970b64ad519f
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-20 10:31:35 +0100

    Cover in-page subscription management drawer

---
+++ src/Tests/Functional/Controllers/Subscriptions/ShopAccountSubscriptionManagementViewTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests\Functional\Controllers\Subscriptions;
+
+use App\Tests\Functional\Controllers\FunctionalTestCase;
+use App\Tests\Unit\Repositories\Concerns\CreatesTestData;
+
+final class ShopAccountSubscriptionManagementViewTest extends FunctionalTestCase
+{
+    use CreatesTestData;
+
+    public function test_manage_action_opens_in_page_drawer_for_current_subscription(): void
+    {
+        $member = $this->createMember();
+        $subscription = $this->createSubscription([
+            'member_id' => $member->id,
+            'status' => 'active',
+            'auto_renew' => false,
+            'plan_name' => 'Daily Digital',
+        ]);
+        $this->actingAsMember($member);
+
+        $response = $this->get('/press-stack/account/subscriptions');
+
+        $this->assertResponseStatus(200, $response);
+        $content = $response->getContent();
+
+        self::assertStringContainsString('id="subscription-manage-drawer"', $content);
+        self::assertStringContainsString('data-open-subscription-manage', $content);
+        self::assertStringContainsString(
+            "/press-stack/account/subscriptions/{$subscription->id}/auto-renew",
+            $content,
+        );
+        self::assertStringNotContainsString(
+            "/{$this->siteSlug}/member/subscriptions",
+            $content,
+        );
+    }
+
+    public function test_previous_subscription_cannot_manage_auto_renew(): void
+    {
+        $member = $this->createMember();
+        $this->createSubscription([
+            'member_id' => $member->id,
+            'status' => 'expired',
+            'auto_renew' => false,
+            'end_date' => date('Y-m-d H:i:s', strtotime('-1 day')),
+        ]);
+        $this->actingAsMember($member);
+
+        $response = $this->get('/press-stack/account/subscriptions');
+
+        $this->assertResponseStatus(200, $response);
+        $content = html_entity_decode($response->getContent(), ENT_QUOTES | ENT_HTML5);
+
+        self::assertStringContainsString('"can_manage_auto_renew":false', $content);
+    }
+}
