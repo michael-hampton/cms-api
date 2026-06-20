e4ebc42b60c72b457baac1a1c662cccdcb637cd2
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-20 11:26:56 +0100

    Add delivery address controls

---
+++ src/views/subscriptions/account/_subscription_delivery_address.php
@@ -0,0 +1,14 @@
+<section id="subscription-delivery-address-section"
+         aria-labelledby="subscription-delivery-address-heading"
+         hidden>
+    <h3 id="subscription-delivery-address-heading">Delivery address</h3>
+    <p>Select the address used for future print deliveries.</p>
+
+    <div id="subscription-delivery-address-list"
+         class="subscription-delivery-address-list"></div>
+
+    <div class="account-message"
+         id="subscription-delivery-address-message"
+         role="alert"
+         aria-live="polite"></div>
+</section>
