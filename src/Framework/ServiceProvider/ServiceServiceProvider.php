c86b085f63c4350f71b1995e74b94cf27edd3486
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-22 08:22:35 +0100

    Bind Stripe subscription gateways in service provider

--- src/Framework/ServiceProvider/ServiceServiceProvider.php
+++ src/Framework/ServiceProvider/ServiceServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Framework\ServiceProvider;
 
+use App\Services\Billing\Stripe\Contracts\StripeSubscriptionGatewayInterface;
+use App\Services\Billing\Stripe\Contracts\StripeSubscriptionScheduleGatewayInterface;
+use App\Services\Billing\Stripe\StripeSubscriptionGateway;
+use App\Services\Billing\Stripe\StripeSubscriptionScheduleGateway;
+
 /**
  * Service Service Provider - Business logic services
  */
@@ -10,5 +15,15 @@ class ServiceServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->autoRegister('services', null, false);
+
+        $this->container->bind(
+            StripeSubscriptionGatewayInterface::class,
+            StripeSubscriptionGateway::class,
+        );
+
+        $this->container->bind(
+            StripeSubscriptionScheduleGatewayInterface::class,
+            StripeSubscriptionScheduleGateway::class,
+        );
     }
 }
