3dae70676806bd5ff794ed0159d81d322d387207
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-22 10:38:46 +0100

    Load account modal acquisition script

--- src/views/subscriptions/account/subscriptions.php
+++ src/views/subscriptions/account/subscriptions.php
@@ -9,16 +9,21 @@
         $_ENV['STRIPE_PUBLIC_KEY'] ?? config('payment.stripe.public_key')
     ) ?>;
 </script>
-<script src="https://js.stripe.com/v3/" defer></script>
-<script src="/public/js/subscription-account.js" defer></script>
-<script src="/public/js/subscription-account-runtime.js" defer></script>
-<script src="/public/js/subscription-account-drawer-bootstrap.js" defer></script>
-<script src="/public/js/subscription-account-management.js" defer></script>
-<script src="/public/js/subscription-account-history-delivery.js" defer></script>
-<script src="/public/js/subscription-account-upgrade.js" defer></script>
-<script src="/public/js/subscription-account-preferences.js" defer></script>
-<script src="/public/js/subscription-account-delivery-address.js" defer></script>
-<script src="/public/js/subscription-account-digital-access.js" defer></script>
-<script src="/public/js/subscription-account-issue-deliveries.js" defer></script>
+<?php foreach ([
+    'https://js.stripe.com/v3/',
+    '/public/js/subscription-account.js',
+    '/public/js/subscription-account-runtime.js',
+    '/public/js/subscription-account-drawer-bootstrap.js',
+    '/public/js/subscription-account-management.js',
+    '/public/js/subscription-account-history-delivery.js',
+    '/public/js/subscription-account-upgrade.js',
+    '/public/js/subscription-account-preferences.js',
+    '/public/js/subscription-account-delivery-address.js',
+    '/public/js/subscription-account-digital-access.js',
+    '/public/js/subscription-account-issue-deliveries.js',
+    '/public/js/subscription-account-acquisition.js',
+] as $script): ?>
+    <script src="<?= htmlspecialchars($script) ?>" defer></script>
+<?php endforeach; ?>
 </body>
 </html>
