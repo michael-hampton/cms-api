b676f16292596bd187e9a2e14d2f42c5f0240894
michael-hampton <michaelhamptondesign@yahoo.com>
2026-06-24 13:05:40 +0100

    Remove duplicate global auth route file

--- src/routes/global-auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controllers\AuthController;
-use App\Framework\Http\Router;
-
-/**
- * @var $router Router
- */
-$router->post('/api/auth/login', [AuthController::class, 'globalLogin']);
